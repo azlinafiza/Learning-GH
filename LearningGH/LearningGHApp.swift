8f4ec43ef84063f7d8ff74fd746573c834967079
Azlin Afiza <azlinafizaabdulaziz@gmail.com>
2021-08-27 23:24:29 +0900

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Azlin Afiza on 2021/08/27.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
